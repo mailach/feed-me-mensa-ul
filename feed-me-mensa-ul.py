76850284842bec1846b458302293d2679d1bd7e4
mailach <mailach@mailbox.org>
2021-11-11 18:40:34 +0100

    Add comments and documentation

--- feed-me-mensa-ul.py
+++ feed-me-mensa-ul.py
@@ -8,11 +8,15 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 CHANNEL_ID = os.environ.get("CHANNEL_ID")
 MAINTAINER_TOKEN = os.environ.get("MAINTAINER_TOKEN")
 MAINTAINER_CHATID = os.environ.get("MAINTAINER_CHATID")
+MACHINE_NAME = os.environ.get("MACHINE_NAME")
 
 def emojify(tag, meal):
+  """This function returns the meal wrapped in two emojis using the specified tag"""
   return "".join([tag]*3)+ meal + "".join([tag]*3)
 
 def get_heading(meal):
+  """This function returns a formatted heading with different emojis depending on content of meal"""
+  
   if "fisch" in meal.lower():
     return emojify(":fish:", meal) 
   elif "fleisch" in meal.lower(): 
@@ -30,51 +34,80 @@ def get_heading(meal):
   elif "salat" in meal.lower(): 
     return emojify(":green_salad:", meal) 
   elif "grill" in meal.lower(): 
-    return emojify(":fire:", meal)     #poultry_leg
+    return emojify(":fire:", meal)    
   elif "pasta" in meal.lower(): 
-    return emojify(":spaghetti:", meal)     
+    return emojify(":spaghetti:", meal)      
+  elif "sättigung" in meal.lower(): 
+    return emojify(":french_fries:", meal)    
   else:
     return meal
 
     
-def get_soup(url):
-    response = requests.get(url)
-    soup = bs4.BeautifulSoup(response.text)
-    return soup
+def get_soup(url: str):
+  """ Returns a Beautifulsoup object from a specified url."""
+  response = requests.get(url)
+  soup = bs4.BeautifulSoup(response.text)
+  return soup
+
+def clean_string(string_to_clean, to_remove):
+  """Takes a string and cleans it from a list of strings and returns cleaned strin. """
+  for ch in to_remove:
+    string_to_clean = string_to_clean.replace(ch, "")
+  return string_to_clean
 
 def get_menu(soup):
-    date = soup.find_all("option", {"selected": "selected"})[1].text
-    menucard = [tag for tag in list(soup.find_all("section", {"class": "meals"})[0]) if tag != "\n"][1:]
-    meal = ""
-    menu = {}
-    for i in range(len(menucard)):
-        tag = menucard[i]
-        if isinstance(tag, Tag):
-            if "title-prim" in list(*tag.attrs.values()):
-                meal = tag.text
-                if meal not in menu:
-                    menu[meal] = {"subtitle": "", "meals":[]}
-            if "meals__subtitle" in list(*tag.attrs.values()):
-                menu[meal]["subtitle"] = tag.text
-            if "accordion" in list(*tag.attrs.values()):
-                items = tag.find_all("section")
-                for item in items: 
-                    name = item.find("h4", {"class": "meals__name"}).text
-                    
-                    price = item.find("p", {"class": "meals__price"}).text.replace("\n", "").replace(" ", "").replace("Preise:", "").replace("/", "   ")
-                    sides = item.find_all("li")
-                    sidedishes = ""
-                    if len(sides) > 0:
-                        sidedishes += "mit "
-                        for s in sides:
-                            sidedishes += s.text + "\n"
-                    menu[meal]["meals"] += [f'{name}\n{sidedishes}{price}\n\n']
+  """Takes a soup object and extracts menu from it. Returns a dictionary with dishtype as keys and corresponding dishes and prices."""
+  
+  # find date by taking selected date in drop down
+  date = soup.find_all("option", {"selected": "selected"})[1].text
+  menucard = [tag for tag in list(soup.find_all("section", {"class": "meals"})[0]) if tag != "\n"][1:]
+  
+  # helper structures for saving menucard information
+  meal = ""
+  menu = {}
 
-    return date, menu
+  # iterate over each tag in the menu section
+  for tag in menucard:
+      if isinstance(tag, Tag):
+          # not all tags have attribute "class", so we go over alle values of attributes to search for specific tag
+          attribute_values = list(*tag.attrs.values())
+          
+          if "title-prim" in attribute_values:
+            # set the meal category 
+              meal = tag.text
+              if meal not in menu:
+                  menu[meal] = {"subtitle": "", "meals":[]}
+          
+          elif "meals__subtitle" in attribute_values:
+              menu[meal]["subtitle"] = tag.text
+          
+          elif "accordion" in attribute_values:
+            # each accordion can hold multiple meals, saved in sections 
+              items = tag.find_all("section")
+            
+            # extract information name, price and sidedish for each meal in the accordion
+              for item in items: 
+                  name = item.find("h4", {"class": "meals__name"}).text
+                  price = clean_string(item.find("p", {"class": "meals__price"}).text, ["\n", " ", "Preise:"]).replace("/", " ")
+                  
+                  # find all sidedish information
+                  sides = item.find_all("li")
+                  sidedishes = ""
 
+                  # only execute if there are any sidedishes
+                  if len(sides) > 0:
+                      sidedishes += "mit "
+                      for s in sides:
+                          sidedishes += s.text + "\n"
+
+                  # finally add our new meal to the corresponding meal category        
+                  menu[meal]["meals"] += [f'{name}\n{sidedishes}{price}\n\n']
+
+  return date, menu
 
 
 def make_message(date, menu, mensa):
+  """ Generates the final message that is send to telegram bot."""
   msg = ""
   msg += f"<b>{mensa}\n{date}\n\n</b>"
     
@@ -83,17 +116,32 @@ def make_message(date, menu, mensa):
     if value["subtitle"] != "":
       msg += f"{value['subtitle']}\n"
     for meal in value["meals"]:
+      
+      # mensa campaigns in smoothie menu -.-
       if "Genießen" not in meal:
-        msg += meal 
+        msg += meal  
+
+    # & breaks api trigger    
     msg = msg.replace("&", "und")
   return msg
 
 try:
+  # generate soup and extract menu
   soup = get_soup('https://www.studentenwerk-leipzig.de/mensen-cafeterien/speiseplan?location=106')
   date, menu = get_menu(soup)
+  # make message and emojize message
   msg = emoji.emojize(make_message(date, menu, "Mensa am Park"))
+  
+  # trigger telegram bot api
   response = requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHANNEL_ID}&text={msg}&parse_mode=html")
+  
+  # check if response is "ok", else raise exception and send response text
+  if response.status_code != 200:
+    raise Exception(f"Did not receive 200 from Telegram API.\n{response.text}")
 
 except Exception as e:
-  message = f"Feed Me Bot failed on reinlach machine.\nError Message:\n{str(e)}"
+
+  # if script fails, send message to maintainer chat
+  message = f"Feed Me Bot script failed on {MACHINE_NAME}.\nError Message:\n{str(e)}"
   response = requests.get(f"https://api.telegram.org/bot{MAINTAINER_TOKEN}/sendMessage?chat_id={MAINTAINER_CHATID}&text={message}&parse_mode=html")
+
